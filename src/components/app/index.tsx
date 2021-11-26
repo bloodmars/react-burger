4d04259a58fa77999cbaaeae2a19a0fb50c9cf8e
Oleg Vovk <vovk911@gmail.com>
2021-11-26 12:32:49 +0300

    sprint-2 step-1

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,17 +1,51 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useReducer } from 'react'
 import styles from './styles.module.css'
 import AppHeader from '../../components/app-header'
 import BurgerConstructor from '../../components/burger-constructor'
 import BurgerIngredients from '../../components/burger-ingredients'
+import IngredientInterface from '../../interfaces/ingredient'
+import UserIngredientsContext from '../../contexts/user-ingredients'
 
-const API_URL = 'https://norma.nomoreparties.space/api/ingredients'
+const INGREDIENTS_API_URL = 'https://norma.nomoreparties.space/api/ingredients'
+
+const userIngredientsPrice = (list: any, side: any) => {
+  let price = 0
+  if (side) {
+    price += side.price * 2
+  }
+  list.map((element: IngredientInterface) => price += element.price)
+  return price
+} 
+
+const userIngredientsReducer = (state: any, action: any) => {
+  if (action.type === 'add') {
+    if (action.payload.type === 'bun') {
+      return { 
+        list: state.list, 
+        side: action.payload, 
+        total: userIngredientsPrice(state.list, action.payload) 
+      }
+    } else {
+      state.list.push(action.payload)
+      return { 
+        list: state.list, 
+        side: state.side,
+        total: userIngredientsPrice(state.list, state.side) 
+      }
+    }
+  }
+}
+
+const userIngredientsInitialState = { list: [], side: undefined, total: 0 }
 
 const App = () => {
   const [ingredients, setIngredients] = useState([])
   const [errorRequest, setErrorRequest] = useState<string>()
+  const [userIngredientsState, userIngredientsDispatch] = useReducer(
+    userIngredientsReducer, userIngredientsInitialState)
   
   useEffect(() => {
-    fetch(API_URL)
+    fetch(INGREDIENTS_API_URL)
       .then(response => {
         if (response.ok) {
           return response.json()
@@ -19,27 +53,21 @@ const App = () => {
           throw new Error('Something went wrong');
         }
       })
-      .then(data => setIngredients(data.data))
+      .then(data => {
+        const items = data.data
+        setIngredients(items)
+
+        // put some ingredients like selected by user for example
+        userIngredientsDispatch({ type: 'add', payload: items[0] })
+        userIngredientsDispatch({ type: 'add', payload: items[5] })
+        userIngredientsDispatch({ type: 'add', payload: items[6] })
+        userIngredientsDispatch({ type: 'add', payload: items[7] })
+        userIngredientsDispatch({ type: 'add', payload: items[8] })
+        userIngredientsDispatch({ type: 'add', payload: items[11] })
+        userIngredientsDispatch({ type: 'add', payload: items[12] })
+      })
       .catch(error => setErrorRequest(error.message))
   }, [])
-
-  const constructorIngredients = React.useMemo(
-    () => {
-      const ingredientsLength = ingredients.length - 1
-      if (ingredientsLength > 0) {
-        return {
-          side: ingredients[0],
-          middle: ingredients.slice(1, ingredientsLength)
-        }
-      } else {
-        return {
-          side: undefined,
-          middle: []
-        }
-      }
-    },
-    [ingredients]
-  )
 
   return (
     <>
@@ -53,11 +81,9 @@ const App = () => {
               />
             </section >
             <section className={`${styles.section} ${styles.right} pt-25 pb-10 pr-4`}>
-              <BurgerConstructor 
-                ingredientTop={constructorIngredients.side}
-                ingredientBottom={constructorIngredients.side}
-                ingredientsMiddle={constructorIngredients.middle}
-              /> 
+              <UserIngredientsContext.Provider value={[userIngredientsState, userIngredientsDispatch]}>
+                <BurgerConstructor /> 
+              </UserIngredientsContext.Provider>
             </section>
           </>
         )}
--- src/components/burger-constructor/index.tsx
+++ src/components/burger-constructor/index.tsx
@@ -1,70 +1,89 @@
-import React, { useState, FC } from 'react'
+import React, { useState, useContext, useReducer, useMemo } from 'react'
 import styles from './styles.module.css';
 import { ConstructorElement, DragIcon, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components'
 import IngredientInterface from '../../interfaces/ingredient'
 import OrderDetails from '../../components/order-details'
 import Modal from '../../components/modal'
+import UserIngredientsContext from '../../contexts/user-ingredients'
 
-interface Props {
-  ingredientTop: IngredientInterface | undefined;
-  ingredientBottom: IngredientInterface | undefined;
-  ingredientsMiddle: IngredientInterface[];
-}
+const ORDER_API_URL = 'https://norma.nomoreparties.space/api/orders'
 
-const BurgerConstructor: FC<Props> = (props) => {
+const BurgerConstructor = () => {
   const [showModal, setShowModal] = useState(false)
   const [orderNumber, setOrderNumber] = useState<string>()
+  const [{list, side, total}, userIngredientsDispatch] = useContext(UserIngredientsContext)
 
-  const showOrderPopup = () => {
-    setOrderNumber('034536')
-    setShowModal(true)
+  const orderHandler = () => {
+    const ingredients = [side].concat(list).concat(side)
+
+    fetch(ORDER_API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ ingredients })
+    })
+    .then(response => {
+      if (response.ok) {
+        return response.json()
+      } else {
+        throw new Error('Something went wrong');
+      }
+    })
+    .then(data => {
+      setOrderNumber(data.order.number)
+      setShowModal(true)
+    })
+    .catch(error => console.log(error.message))
   }
 
   return (
     <>
-      {props.ingredientTop && (
+      {side && (
         <ConstructorElement
           type="top"
           isLocked={true}
-          text={props.ingredientTop.name}
-          price={props.ingredientTop.price}
-          thumbnail={props.ingredientTop.image}
+          text={`${side.name} (верх)`}
+          price={side.price}
+          thumbnail={side.image}
         />         
       )}
 
       <div className={styles.scroller}>
-        {props.ingredientsMiddle.map((element) => {
+        {list.map((ingredient: IngredientInterface) => {
           return (
-            <div className={styles.draggable} key={element._id}>
+            <div className={styles.draggable} key={ingredient._id}>
               <DragIcon type="primary" />
               <ConstructorElement
                 isLocked={false}
-                text={element.name}
-                price={element.price}
-                thumbnail={element.image}
+                text={ingredient.name}
+                price={ingredient.price}
+                thumbnail={ingredient.image}
               />  
             </div>
           )
         })}
       </div>
 
-      {props.ingredientBottom && (
+      {side && (
         <ConstructorElement
           type="bottom"
           isLocked={true}
-          text={props.ingredientBottom.name}
-          price={props.ingredientBottom.price}
-          thumbnail={props.ingredientBottom.image}
+          text={`${side.name} (низ)`}
+          price={side.price}
+          thumbnail={side.image}
         />
       )}
 
-      <div className={`${styles.total} mt-10`}>
-        <div className={`${styles.price} mr-10`}>
-          <span className="text text_type_digits-medium">610</span>
-          <CurrencyIcon type="primary" />
-        </div>
-        <Button type="primary" size="large" onClick={showOrderPopup}>Оформить заказ</Button>
-      </div>
+      {side && list.length > 0 && (
+        <div className={`${styles.total} mt-10`}>
+          <div className={`${styles.price} mr-10`}>
+            <span className="text text_type_digits-medium">{total}</span>
+            <CurrencyIcon type="primary" />
+          </div>
+          <Button type="primary" size="large" onClick={orderHandler}>Оформить заказ</Button>
+        </div>        
+      )}
 
       {showModal && orderNumber && (
         <Modal onClose={() => setShowModal(false)}>
